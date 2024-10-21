46f53d166cbeae9a3340bc58ce7488f8218923d8
Mel77O <melchiah.opleda@cit.edu>
2024-10-21 19:45:45 +0800

    calendar

--- Projectyey/src/userpages/calendar.jsx
+++ Projectyey/src/userpages/calendar.jsx
@@ -50,29 +50,29 @@ const App = () => {
 
   useEffect(() => {
     const studentName = localStorage.getItem('studentName');
-    const studentIdNumber = localStorage.getItem('studentIdNumber');
+    const studentIdNumber = localStorage.getItem('studentIdNumber'); 
 
     // Debugging logs to check values
-    console.log('Fetched studentName:', studentName);
+    console.log('Fetched studentName:', studentName); 
     console.log('Fetched studentIdNumber:', studentIdNumber);
 
-  }, []);
+}, []);
 
   useEffect(() => {
     const now = new Date();
 
     fetch('http://localhost:8080/api/events')
-        .then((response) => response.json())
-        .then((data) => {
-          const formattedEvents = data.map((event) => ({
-            ...event,
-            start: new Date(event.start),
-            end: new Date(event.end),
-            type: event.isBooked || new Date(event.end) < now ? 'Unavailable' : event.type,
-          }));
-          setEvents(formattedEvents);
-        })
-        .catch((error) => console.error('Error fetching events:', error));
+      .then((response) => response.json())
+      .then((data) => {
+        const formattedEvents = data.map((event) => ({
+          ...event,
+          start: new Date(event.start),
+          end: new Date(event.end),
+          type: event.isBooked || new Date(event.end) < now ? 'Unavailable' : event.type,
+        }));
+        setEvents(formattedEvents);
+      })
+      .catch((error) => console.error('Error fetching events:', error));
   }, []);
 
   // Only open modal for "Available" events
@@ -97,20 +97,20 @@ const App = () => {
   const handleReserve = async () => {
     const studentName = localStorage.getItem('studentName');
     const studentIdNumber = localStorage.getItem('studentIdNumber');
-
+  
     if (!studentName) {
       alert("You need to log in to reserve a slot.");
       navigate('/login-student');
       return;
     }
-
+  
     try {
       // Fetch student data using studentIdNumber
       const response = await fetch(`http://localhost:8080/student/students/${studentIdNumber}`);
       if (!response.ok) {
         throw new Error('Failed to fetch student data');
       }
-
+  
       const studentData = await response.json();
 
       // Fetch the student's existing reservations
@@ -119,10 +119,10 @@ const App = () => {
 
       if (reservations.length > 0) {
         setLimitModalOpen(true);
-        return;
+        return; 
       }
-
-
+  
+  
       // Create the reservation request with fetched data
       const reservationRequest = {
         studentIdNumber: studentData.idNumber,
@@ -133,25 +133,25 @@ const App = () => {
         date: moment(selectedEvent.start).format('YYYY-MM-DD'),
         time: `${moment(selectedEvent.start).format('h:mm A')} - ${moment(selectedEvent.end).format('h:mm A')}`,
       };
-
+  
       console.log('Reservation Request:', reservationRequest);
-
+  
       // Post reservation
       const reserveResponse = await fetch('http://localhost:8080/api/reservations/reserve', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(reservationRequest),
       });
-
+  
       if (!reserveResponse.ok) {
         const errorData = await reserveResponse.json();
-        throw new Error(errorData.error);
+        throw new Error(errorData.error); 
       }
-
+  
       const data = await reserveResponse.json();
       console.log('Reservation successful:', data.message);
-      setModalOpen(false);
-      setSuccessModalOpen(true);
+      setModalOpen(false); 
+      setSuccessModalOpen(true);  
 
       setReservationDetails({
         date: moment(selectedEvent.start).format('MMMM Do YYYY'),
@@ -159,49 +159,49 @@ const App = () => {
         status: 'Reserved',
       });
   
-      // Update the event status on the backend
-      await fetch(`http://localhost:8080/api/events/book/${selectedEvent.id}`, {
-       method: 'PUT',
-       headers: { 'Content-Type': 'application/json' }
-     });
- 
-     // Update the event list to mark the event as unavailable
-     setEvents((prevEvents) =>
-       prevEvents.map((event) =>
-         event.id === selectedEvent.id
-           ? { ...event, type: 'Unavailable' } // Set to 'Unavailable'
-           : event
-       )
-     );
+     // Update the event status on the backend
+     await fetch(`http://localhost:8080/api/events/book/${selectedEvent.id}`, {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' }
+    });
 
+    // Update the event list to mark the event as unavailable
+    setEvents((prevEvents) =>
+      prevEvents.map((event) =>
+        event.id === selectedEvent.id
+          ? { ...event, type: 'Unavailable' } // Set to 'Unavailable'
+          : event
+      )
+    );
+  
     } catch (error) {
       console.error('Error reserving slot:', error.message || error);
-      setModalOpen(false);
-      setLimitModalOpen(true);
+      setModalOpen(false); 
+      setLimitModalOpen(true);  
     }
   };
 
 
-
+  
 
   const SuccessModal = () => {
     if (!successModalOpen) return null;
 
     return ReactDOM.createPortal(
-        <div className="fixed inset-0 z-30 flex items-center justify-center overflow-y-auto text-black bg-black bg-opacity-50">
-          <div ref={wrapperRef} style={{ ...modalStyles.content, padding: '20px' }}>
-            <h2 className="text-xl font-bold text-center">Yeyy Reservation Successful!</h2>
-            <div className="flex justify-center space-x-4 mt-4">
-              <Button variant="contained" style={{ backgroundColor: '#F7C301' }} onClick={closeSuccessModal}>
-                Close
-              </Button>
-            </div>
+      <div className="fixed inset-0 z-30 flex items-center justify-center overflow-y-auto text-black bg-black bg-opacity-50">
+        <div ref={wrapperRef} style={{ ...modalStyles.content, padding: '20px' }}>
+          <h2 className="text-xl font-bold text-center">Yeyy Reservation Successful!</h2>
+          <div className="flex justify-center space-x-4 mt-4">
+            <Button variant="contained" style={{ backgroundColor: '#F7C301' }} onClick={closeSuccessModal}>
+              Close
+            </Button>
           </div>
-        </div>,
-        document.body
+        </div>
+      </div>,
+      document.body
     );
   };
-
+  
 
 
   const ModalComponent = () => {
@@ -211,28 +211,28 @@ const App = () => {
     const eventTime = `${moment(selectedEvent.start).format('h:mm A')} - ${moment(selectedEvent.end).format('h:mm A')}`;
 
     return ReactDOM.createPortal(
-        <div className="fixed inset-0 z-30 flex items-center justify-center overflow-y-auto text-black bg-black bg-opacity-50" >
-          <div ref={wrapperRef} style={{ ...modalStyles.content, padding: '20px' }}>
-            <h2 className="text-xl font-bold text-center">Confirm Reservation</h2>
-            <p className="mt-2 text-center">Are you sure you want to reserve this slot?</p>
-            <p className="mt-2 text-center">
-              <strong>Date:</strong> {eventDate}
-            </p>
-            <p className="mt-2 text-center">
-              <strong>Time:</strong> {eventTime}
-            </p>
-            <div className="flex justify-end space-x-4 mt-4">
-              <Button
-                  variant="contained" style={{ backgroundColor: '#88343B' }} onClick={closeModal}>
-                Cancel
-              </Button>
-              <Button variant="contained" style={{ backgroundColor: '#F7C301' }} onClick={handleReserve}>
-                Confirm
-              </Button>
-            </div>
+      <div className="fixed inset-0 z-30 flex items-center justify-center overflow-y-auto text-black bg-black bg-opacity-50" >
+        <div ref={wrapperRef} style={{ ...modalStyles.content, padding: '20px' }}>
+          <h2 className="text-xl font-bold text-center">Confirm Reservation</h2>
+          <p className="mt-2 text-center">Are you sure you want to reserve this slot?</p>
+          <p className="mt-2 text-center">
+            <strong>Date:</strong> {eventDate}
+          </p>
+          <p className="mt-2 text-center">
+            <strong>Time:</strong> {eventTime}
+          </p>
+          <div className="flex justify-end space-x-4 mt-4">
+            <Button 
+            variant="contained" style={{ backgroundColor: '#88343B' }} onClick={closeModal}>
+              Cancel
+            </Button>
+            <Button variant="contained" style={{ backgroundColor: '#F7C301' }} onClick={handleReserve}>
+              Confirm
+            </Button>
           </div>
-        </div>,
-        document.body
+        </div>
+      </div>,
+      document.body
     );
   };
 
@@ -271,24 +271,25 @@ const App = () => {
 
   const LimitModal = () => {
     if (!limitModalOpen) return null;
-
+  
     return ReactDOM.createPortal(
-        <div className="fixed inset-0 z-30 flex items-center justify-center overflow-y-auto text-black bg-black bg-opacity-50">
-          <div ref={wrapperRef} style={{ ...modalStyles.content, padding: '20px' }}>
-            <h2 className="text-xl font-bold text-center">Reservation Limit Reached</h2>
-            <p className="mt-2 text-center">You are limited to 1 available reservation slot only!</p>
-            <div className="flex justify-center space-x-4 mt-4">
-              <Button variant="contained" style={{ backgroundColor: '#90242c' }} onClick={() => setLimitModalOpen(false)}>
-                Close
-              </Button>
-            </div>
+      <div className="fixed inset-0 z-30 flex items-center justify-center overflow-y-auto text-black bg-black bg-opacity-50">
+        <div ref={wrapperRef} style={{ ...modalStyles.content, padding: '20px' }}>
+          <h2 className="text-xl font-bold text-center">Reservation Limit Reached</h2>
+          <p className="mt-2 text-center">You are limited to 1 available reservation slot only!</p>
+          <div className="flex justify-center space-x-4 mt-4">
+            <Button variant="contained" style={{ backgroundColor: '#90242c' }} onClick={() => setLimitModalOpen(false)}>
+              Close
+            </Button>
           </div>
-        </div>,
-        document.body
+        </div>
+      </div>,
+      document.body
     );
   };
-  // Fetch student data and reserved event
-  useEffect(() => {
+
+   // Fetch student data and reserved event
+   useEffect(() => {
     const fetchReservedEvent = async () => {
       const studentIdNumber = localStorage.getItem('studentIdNumber'); // Retrieve student ID from localStorage
       console.log("Fetched studentIdNumber:", studentIdNumber); // Debugging
@@ -354,21 +355,22 @@ const App = () => {
       alert('No reservation to cancel.');
     }
   };
+  
 
   return (
-      <div>
-        <Studnav />
-        <ModalComponent />
-        <SuccessModal />
-        <LimitModal />
+    <div>
+      <Studnav />
+      <ModalComponent />
+      <SuccessModal />
+      <LimitModal />
 
-        <div className="isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
-          <div className="mx-auto max-w-7xl text-center">
-            <h1 className="text-4xl font-bold tracking-tight text-[#88343B] sm:text-5xl">Choose an Appointment</h1>
-          </div>
+      <div className="isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
+        <div className="mx-auto max-w-7xl text-center">
+          <h1 className="text-4xl font-bold tracking-tight text-[#88343B] sm:text-5xl">Choose an Appointment</h1>
+        </div>
 
-         {/* Reserved Event Display */}
-         <div className="mx-auto max-w-xl mt-10 p-6 rounded-lg text-white" style={{ backgroundColor: '#88343b' }}>
+        {/* Reserved Event Display */}
+        <div className="mx-auto max-w-xl mt-10 p-6 rounded-lg text-white" style={{ backgroundColor: '#88343b' }}>
           {reservedEvent ? (
             <Box>
               <Typography variant="h5" component="h2" gutterBottom>
@@ -398,39 +400,39 @@ const App = () => {
         
 
 
-          <div className="isolate bg-white px-6 py-24 sm:py-32 lg:px-8 rounded-lg text-black">
-            <Box sx={{ display: 'flex', minHeight: '100vh' }}>
-              <Box sx={{ flexGrow: 1, p: 3 }}>
-                {/* Color Legend */}
-                <Box sx={{ display: 'flex', alignItems: 'center', mb: 2, color: 'black' }}>
-                  <Box sx={{ backgroundColor: '#FDE74C', width: 20, height: 20, mr: 1 }} />
-                  <Typography variant="body2" sx={{ mr: 2 }}>Available Slot</Typography>
-                  <Box sx={{ backgroundColor: '#b8bcc4', width: 20, height: 20, mr: 1 }} />
-                  <Typography variant="body2" sx={{ mr: 2 }}>Unavailable Slot</Typography>
-                  <Box sx={{ backgroundColor: '#cc9999', width: 20, height: 20, mr: 1 }} />
-                  <Typography variant="body2">Holiday</Typography>
-                </Box>
-
-                {/* Calendar */}
-                <Calendar
-                    localizer={localizer}
-                    events={events}
-                    startAccessor="start"
-                    endAccessor="end"
-                    style={{ color: 'black', backgroundColor: 'white' }}
-                    selectable
-                    onSelectEvent={handleEventClick}
-                    views={['month', 'week', 'day']}
-                    defaultView="month"
-                    eventPropGetter={eventStyleGetter}
-                />
+        <div className="isolate bg-white px-6 py-24 sm:py-32 lg:px-8 rounded-lg text-black">
+          <Box sx={{ display: 'flex', minHeight: '100vh' }}>
+            <Box sx={{ flexGrow: 1, p: 3 }}>
+              {/* Color Legend */}
+              <Box sx={{ display: 'flex', alignItems: 'center', mb: 2, color: 'black' }}>
+                <Box sx={{ backgroundColor: '#FDE74C', width: 20, height: 20, mr: 1 }} />
+                <Typography variant="body2" sx={{ mr: 2 }}>Available Slot</Typography>
+                <Box sx={{ backgroundColor: '#b8bcc4', width: 20, height: 20, mr: 1 }} />
+                <Typography variant="body2" sx={{ mr: 2 }}>Unavailable Slot</Typography>
+                <Box sx={{ backgroundColor: '#cc9999', width: 20, height: 20, mr: 1 }} />
+                <Typography variant="body2">Holiday</Typography>
               </Box>
-            </Box>
-          </div>
-        </div>
 
-        <Studfooter />
+              {/* Calendar */}
+              <Calendar
+                localizer={localizer}
+                events={events}
+                startAccessor="start"
+                endAccessor="end"
+                style={{ color: 'black', backgroundColor: 'white' }}
+                selectable
+                onSelectEvent={handleEventClick}
+                views={['month', 'week', 'day']}
+                defaultView="month"
+                eventPropGetter={eventStyleGetter}
+              />
+            </Box>
+          </Box>
+        </div>
       </div>
+
+      <Studfooter />
+    </div>
   );
 };
 
