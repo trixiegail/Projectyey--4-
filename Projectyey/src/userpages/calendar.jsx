f11ffcc0ac71d4453c9ba3eacadbf9cd9a6a289a
Mel77O <melchiah.opleda@cit.edu>
2024-12-05 04:05:55 +0800

    reservation page reload

--- Projectyey/src/userpages/calendar.jsx
+++ Projectyey/src/userpages/calendar.jsx
@@ -91,6 +91,7 @@ const App = () => {
 
   const closeSuccessModal = () => {
     setSuccessModalOpen(false);
+    window.location.reload();
   };
 
   const handleReserve = async () => {
@@ -363,7 +364,7 @@ const App = () => {
       alert('No reservation to cancel.');
     }
   };
-///////////////////////////////////////
+
   const CancelConfirmationModal = () => {
     if (!openCancelConfirmModal) return null;
 
@@ -376,16 +377,17 @@ const App = () => {
             <Button 
               variant="contained" 
               style={{ backgroundColor: '#88343B' }} 
-              onClick={() => setOpenCancelConfirmModal(false)} // Close the modal
+              onClick={() => setOpenCancelConfirmModal(false)} 
             >
               No, Keep Reservation
             </Button>
             <Button 
   variant="contained" 
-  style={{ backgroundColor: '#F7C301', color: '#88343B', fontWeight: 'bold' }} // Set text color to maroon and make it bold
+  style={{ backgroundColor: '#F7C301', color: '#88343B', fontWeight: 'bold' }} 
   onClick={() => {
-    handleCancelReservation(); // Call the existing cancellation function
-    setOpenCancelConfirmModal(false); // Close the modal
+    handleCancelReservation(); 
+    setOpenCancelConfirmModal(false); 
+    window.location.reload();
   }}
 >
   Yes, Cancel Reservation
