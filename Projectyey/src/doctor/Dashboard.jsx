8eb58df69c5271faf8318e3db28beee1a626ecd9
Mel77O <melchiah.opleda@cit.edu>
2024-12-05 02:34:16 +0800

    dashboard update4

--- Projectyey/src/doctor/Dashboard.jsx
+++ Projectyey/src/doctor/Dashboard.jsx
@@ -317,7 +317,7 @@ const Dashboard = () => {
         </Grid>
 
 
-        <Grid container spacing={3} mt={-30}>
+        <Grid container spacing={3} mt={-35}>
   {/* Left Column - Completed Patients Statistics */}
   <Grid item xs={12} md={6}>
     <Card sx={{ p: 3 }}>
