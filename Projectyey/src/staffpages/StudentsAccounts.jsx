7b88f59eca01467fcd7ef3619bbb38ef1f0a4f6a
trixie <trixiegail.cloma@cit.edu>
2024-12-05 11:02:15 +0800

    student accountsss

--- Projectyey/src/staffpages/StudentsAccounts.jsx
+++ Projectyey/src/staffpages/StudentsAccounts.jsx
@@ -91,12 +91,12 @@ function StudentsAccounts() {
   };
 
   const handleUpdate = async () => {
-    console.log('Selected User before Update:', selectedUser);
+    console.log('Selected User before Update:', selectedUser.id);
 
     if (selectedUser && selectedUser.id) {
       try {
         console.log('Updating user with ID:', selectedUser.id);
-        const response = await axios.put(`https://dentalmanagement.azurewebsites.net/student/updateStudent/${selectedUser.id}`, selectedUser);
+        const response = await axios.put(`https://dentalmanagement.azurewebsites.net/student/updateStudent/${selectedUser.id}`, selectedUser.id);
         if (response.status === 200) {
           console.log('Student account updated successfully');
           fetchData();
