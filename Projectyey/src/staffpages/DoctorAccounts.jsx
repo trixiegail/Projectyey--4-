99ad6f9b4b09dbd002c319ce4e13e6af60f30cf3
trixie <trixiegail.cloma@cit.edu>
2024-12-05 10:38:53 +0800

    binalik sa dati ang doctor accounts

--- Projectyey/src/staffpages/DoctorAccounts.jsx
+++ Projectyey/src/staffpages/DoctorAccounts.jsx
@@ -150,8 +150,6 @@ function DoctorAccounts() {
                 <th className="w-1/6 py-3 px-4 uppercase font-semibold text-sm">Last Name</th>
                 <th className="w-1/6 py-3 px-4 uppercase font-semibold text-sm">Birthdate</th>
                 <th className="w-1/6 py-3 px-4 uppercase font-semibold text-sm">Email</th>
-                <th className="w-1/6 py-3 px-4 uppercase font-semibold text-sm">Email</th>
-                <th className="w-1/6 py-3 px-4 uppercase font-semibold text-sm">Email</th>
                 <th className="w-1/6 py-3 px-4 uppercase font-semibold text-sm">Actions</th>
               </tr>
             </thead>
