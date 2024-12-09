a2b4ac0e8913fc9985ff9885e4543c4208a379b1
trixie <trixiegail.cloma@cit.edu>
2024-12-09 08:41:49 +0800

    get all students

--- Projectyey/src/staffpages/StudentsAccounts.jsx
+++ Projectyey/src/staffpages/StudentsAccounts.jsx
@@ -16,7 +16,7 @@ function StudentAccounts() {
 
   const fetchData = async () => {
     try {
-      const response = await axios.get('https://dentalmanagement.azurewebsites.net/student/getStudents?archived=false');
+      const response = await axios.get('https://dentalmanagement.azurewebsites.net/student/getAllStudents?archived=false');
       if (response.status === 200) {
         setData(response.data);
         console.log('Doctor accounts fetched successfully:', response.data);
