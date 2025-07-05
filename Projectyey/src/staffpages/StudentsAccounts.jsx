9d87f0464536ff1623060ec2238832a85b518681
trixie <trixiegail.cloma@cit.edu>
2025-07-05 16:27:28 +0800

    adjusted the create student account

--- Projectyey/src/staffpages/StudentsAccounts.jsx
+++ Projectyey/src/staffpages/StudentsAccounts.jsx
@@ -136,7 +136,7 @@ function StudentAccounts() {
             Search
           </button>
           <a
-            href="/create-doctor-account"
+            href="/create-student-account"
             className="ml-2 p-2 bg-[#F7C301] text-white rounded-lg shadow-md hover:bg-[#F7C301]"
           >
             <span>Create Account</span>
